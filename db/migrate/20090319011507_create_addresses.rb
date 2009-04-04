7d6e22f024136f6a641da01ca66d5cfac878a08a
Marcos Augusto <x@nofxx.com>
2009-04-04 08:09:15 -0300

    mais gis

--- db/migrate/20090319011507_create_addresses.rb
+++ db/migrate/20090319011507_create_addresses.rb
@@ -2,17 +2,20 @@ class CreateAddresses < ActiveRecord::Migration
   def self.up
     create_table :addresses do |t|
       t.references :addressable, :polymorphic => true
-      t.references :city
-      t.string :street
-      t.string :complement
-      t.string :number
-      t.string :zip_code
+      t.references :city, :null => false
+      t.references :zone
+      t.string :street, :complement, :number, :zip_code
+      t.point :geom, :srid => 4326
 
       t.timestamps
     end
 
     add_index :addresses, [:addressable_type, :addressable_id]
+    add_index :addresses, :addressable_type
     add_index :addresses, :city_id
+    add_index :addresses, :geom, :spatial => true
+    add_index :addresses, :zip_code
+    add_index :addresses, :street
   end
 
   def self.down
--- db/migrate/20090319011542_create_cities.rb
+++ db/migrate/20090319011542_create_cities.rb
@@ -1,15 +1,21 @@
 class CreateCities < ActiveRecord::Migration
   def self.up
     create_table :cities do |t|
-      t.references :country
+      t.references :country, :null => false
       t.references :province
-      t.string :name
-      t.integer :pop
+      t.string :name, :permalink, :null => false
+      t.integer :pop, :null => false, :default => 1
+      t.polygon :geom, :srid => 4326
 
       t.timestamps
     end
 
+    add_index :cities, :province_id
+    add_index :cities, :country_id
     add_index :cities, :name
+    add_index :cities, :permalink
+    add_index :cities, :pop
+    add_index :cities, :geom, :spatial => true
   end
 
   def self.down
