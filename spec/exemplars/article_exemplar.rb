ec3a626197b8d866f45539b376e991a8ec6aaddf
Marcos Augusto <x@nofxx.com>
2009-04-06 16:19:46 -0300

    efeito 3d e melhores exemplars

--- spec/exemplars/article_exemplar.rb
+++ spec/exemplars/article_exemplar.rb
@@ -1,8 +1,17 @@
 class Article < ActiveRecord::Base
-  generator_for :title, Faker::Lorem.sentence
-  generator_for :body, Faker::Lorem.paragraph
   generator_for :public, true
+  generator_for :title, :method => :fake_title
+  generator_for :body, :method => :fake_body
   generator_for :published, true
 
+  def self.fake_title
+    Faker::Lorem.sentence
+  end
+
+  def self.fake_body
+    out = Faker::Lorem.paragraph
+    rand(5).times { out += Faker::Lorem.paragraph }
+    out
+  end
 end
 
--- spec/exemplars/publication_exemplar.rb
+++ spec/exemplars/publication_exemplar.rb
@@ -1,6 +1,15 @@
 class Publication < ActiveRecord::Base
-  generator_for :title, "Mega Massa"
-  generator_for :body, "value for body"
+  generator_for :title, :method => :fake_title
+  generator_for :body, :method => :fake_body
   generator_for :published, true
 
+  def self.fake_title
+    Faker::Lorem.sentence
+  end
+
+  def self.fake_body
+    out = Faker::Lorem.paragraph
+    rand(5).times { out += Faker::Lorem.paragraph }
+    out
+  end
 end
